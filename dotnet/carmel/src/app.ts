cbc2b9dd0fbbbefc29052089ac29567e5f751114
Daniel Bendel <daniel.bendel@gmail.com>
2017-03-11 12:52:02 +0100

    refactor(app): remove jquery code and comments

--- dotnet/carmel/src/app.ts
+++ dotnet/carmel/src/app.ts
@@ -1,23 +1,2 @@
-declare var $: any;
+export class App { }
 
-export class App {
-  message = 'Hello World!';
-  //sidebar: Element;
-  //sidebarToggle: HTMLButtonElement;
-  //wrapper: Element;
-
-  attached() { }
-
-  //toggleSidebar() {
-  //  const icon = this.sidebarToggle.querySelector('i.fa');
-  //  this.sidebar.classList.toggle('hide-sidebar');
-  //  this.wrapper.classList.toggle('hide-sidebar');
-  //  if (this.sidebar.classList.contains('hide-sidebar')) {
-  //    icon.classList.remove('fa-angle-left');
-  //    icon.classList.add('fa-angle-right');
-  //  } else {
-  //    icon.classList.remove('fa-angle-right');
-  //    icon.classList.add('fa-angle-left');
-  //  }
-  //}
-}
