2e8b883236475714a6bc8d6b26aa3f1f55285f5c
Daniel Bendel <daniel.bendel@gmail.com>
2017-03-11 13:05:19 +0100

    feat(app): add routing

--- dotnet/carmel/src/app.ts
+++ dotnet/carmel/src/app.ts
@@ -1,2 +1,14 @@
-export class App { }
+import { Router, RouterConfiguration } from 'aurelia-router';
 
+export class App {
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'Carmel';
+    config.map([
+      { route: '', moduleId: 'views/index', title: 'Home Page' }
+    ]);
+
+    this.router = router;
+  }
+}
